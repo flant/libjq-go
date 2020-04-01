86d6f220ab8b69dedf20ae91f5008a2ecea37350
Ivan Mikheykin <ivan.mikheykin@flant.com>
2020-04-01 12:13:53 +0300

    fix: build script out directory

--- scripts/build-libjq-static.sh
+++ scripts/build-libjq-static.sh
@@ -3,9 +3,9 @@
 root=$1
 out=$2
 
-mkdir -p ${out}/libjq
+mkdir -p ${out}
 
-echo "Build jq library into ${out}/libjq"
+echo "Build jq library into '${out}'"
 
 cd ${root}/modules/jq
 
@@ -18,16 +18,17 @@ autoreconf -fi
             --disable-docs \
             --disable-valgrind \
             --with-oniguruma=builtin \
-            --prefix=${out}/libjq
+            --prefix=${out}
+
 make
 make install-libLTLIBRARIES install-includeHEADERS
 
 echo Copy libonig
 
-cp modules/oniguruma/src/.libs/libonig.a ${out}/libjq/lib
-cp modules/oniguruma/src/.libs/libonig.la ${out}/libjq/lib
-cp modules/oniguruma/src/.libs/libonig.lai ${out}/libjq/lib
+cp modules/oniguruma/src/.libs/libonig.a ${out}/lib
+cp modules/oniguruma/src/.libs/libonig.la ${out}/lib
+cp modules/oniguruma/src/.libs/libonig.lai ${out}/lib
 
 echo "Use these flags with go build:"
-echo "CGO_CFLAGS=-I${out}/libjq/include"
-echo "CGO_LDFLAGS=-L${out}/libjq/lib"
+echo "CGO_CFLAGS=-I${out}/include"
+echo "CGO_LDFLAGS=-L${out}/lib"
